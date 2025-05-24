0bcdb4a4c6c3d9f18a053d1882d3566f14efd246
bogdanovich-kirill <crazy.anonymous50@proton.me>
2025-05-24 23:00:53 +0300

    Добавил установку Chrome в GitHub Actions

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,18 @@
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # без GUI
+    chrome_options.add_argument("--no-sandbox")  # для CI
+    chrome_options.add_argument("--disable-dev-shm-usage")  # для CI
+    chrome_options.add_argument("--disable-gpu")  # на всякий случай
+    chrome_options.add_argument("--window-size=1920,1080")  # стабильное разрешение
+    # НЕ добавлять chrome_options.add_argument("--user-data-dir=...")
+    
+    driver = webdriver.Chrome(options=chrome_options)
     yield driver
     driver.quit()
