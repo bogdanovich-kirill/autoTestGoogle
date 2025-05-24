0ba959c106ab565c17f2701c378c1c4579ea9606
bogdanovich-kirill <crazy.anonymous50@proton.me>
2025-05-24 19:10:33 +0300

    Добавлен тест на кнопку поиска

---
+++ test_selenium_google_searchBtn.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# Запускаем браузер
+driver = webdriver.Chrome()
+
+driver.get("https://www.google.com")
+
+# Находим поле ввода
+search_box = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.NAME, "q"))
+)
+search_box.send_keys("Selenium Python")
+
+# Ждем, пока кнопка станет кликабельной
+search_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.NAME, "btnK"))
+)
+search_button.click()
+
+# Ждем, чтобы увидеть результаты
+time.sleep(5)
+
+# Закрываем браузер
+driver.quit()
